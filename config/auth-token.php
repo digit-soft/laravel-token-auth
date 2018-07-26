3ff3053d15ff69824ab4f575e3d2c8d4ec5cfe1f
digit <digit.vova@gmail.com>
2018-07-26 12:26:54 +0300

    Config changes

--- config/auth-token.php
+++ config/auth-token.php
@@ -1,9 +1,10 @@
 <?php
 
 return array(
-    'storage' => 'redis',
-    'ttl' => 60,
+    'storage_class' => 'DigitSoft\LaravelTokenAuth\Storage\Redis',
+    'token_class' => 'DigitSoft\LaravelTokenAuth\AccessToken',
+    'ttl' => 5,
     'connection' => null,
-    'token_prefix' => 'tkn_',
-    'user_prefix' => 'usr_',
+    'token_prefix' => 'tkn:',
+    'user_prefix' => 'usr:',
 );
