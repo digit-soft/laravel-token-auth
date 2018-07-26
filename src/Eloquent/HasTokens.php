b2ce82d38f6b15f4435ba27045cf6991ac98a6ca
digit <digit.vova@gmail.com>
2018-07-26 13:45:06 +0300

    Add user trait

---
+++ src/Eloquent/HasTokens.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace DigitSoft\LaravelTokenAuth\Eloquent;
+
+use DigitSoft\LaravelTokenAuth\AccessToken;
+use DigitSoft\LaravelTokenAuth\Contracts\Storage;
+
+trait HasTokens
+{
+    /**
+     * Get user active access tokens
+     * @return AccessToken[]
+     */
+    public function getTokens()
+    {
+        $pk = $this->primaryKey;
+        return $this->getTokensStorage()->getUserTokens($pk, true);
+    }
+
+    /**
+     * Get active token
+     * @param string|null $client_id
+     * @return AccessToken
+     */
+    public function getToken($client_id = null)
+    {
+        $client_id = $client_id ?? AccessToken::CLIENT_ID_DEFAULT;
+        $token = AccessToken::getFirstFor($this, $client_id);
+        $token = $token ?? $this->createToken($client_id);
+        return $token;
+    }
+
+    /**
+     * Create new access token and save it to storage
+     * @param string|null $client_id
+     * @param int|null    $ttl
+     * @return AccessToken
+     */
+    public function createToken($client_id = null, $ttl = 0)
+    {
+        $token = AccessToken::createFor($this, $client_id);
+        if ($ttl !== 0) {
+            $token->setTtl($ttl);
+        }
+        $token->save();
+        return $token;
+    }
+
+    /**
+     * Get token storage instance
+     * @return Storage
+     */
+    protected function getTokensStorage()
+    {
+        return app('auth.tokencached.storage');
+    }
+}
