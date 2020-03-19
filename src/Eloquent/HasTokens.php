7b7cd507ae353d404a14ad9d162d0e75be97fcde
digit <digit.vova@gmail.com>
2020-03-19 14:54:36 +0200

    CS

--- src/Eloquent/HasTokens.php
+++ src/Eloquent/HasTokens.php
@@ -2,25 +2,28 @@
 
 namespace DigitSoft\LaravelTokenAuth\Eloquent;
 
+use DigitSoft\LaravelTokenAuth\Contracts\Storage;
 use DigitSoft\LaravelTokenAuth\Facades\TokenCached;
 use DigitSoft\LaravelTokenAuth\Contracts\AccessToken as AccessTokenContract;
-use DigitSoft\LaravelTokenAuth\Contracts\Storage;
 
 trait HasTokens
 {
     /**
-     * Get user active access tokens
+     * Get user active access tokens.
+     *
      * @return AccessTokenContract[]
      */
     public function getTokens()
     {
         $id = $this->getAuthIdentifier();
+
         return $this->getTokensStorage()->getUserTokens($id, true);
     }
 
     /**
-     * Get active token
-     * @param string|null $client_id
+     * Get first active token.
+     *
+     * @param  string|null $client_id
      * @return AccessTokenContract
      */
     public function getToken($client_id = null)
@@ -28,13 +31,15 @@ trait HasTokens
         $client_id = $client_id ?? $this->getClientIdFromRequest();
         $token = TokenCached::getFirstFor($this, $client_id);
         $token = $token ?? $this->createToken($client_id);
+
         return $token;
     }
 
     /**
-     * Create new access token and save it to storage
-     * @param string|null $client_id
-     * @param int|null    $ttl
+     * Create new access token and save it to storage.
+     *
+     * @param  string|null $client_id
+     * @param  int|null    $ttl
      * @return AccessTokenContract
      */
     public function createToken($client_id = null, $ttl = 0)
@@ -44,11 +49,13 @@ trait HasTokens
             $token->setTtl($ttl);
         }
         $token->save();
+
         return $token;
     }
 
     /**
-     * Get token storage instance
+     * Get token storage instance.
+     *
      * @return Storage
      */
     protected function getTokensStorage()
@@ -57,7 +64,8 @@ trait HasTokens
     }
 
     /**
-     * Get client ID from request
+     * Get client ID from request.
+     *
      * @return string
      */
     protected function getClientIdFromRequest()
